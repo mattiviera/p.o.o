b47e5bd1d5acb100ce98f6c77a01971fe4ec2726
mattiviera <matiiviera38@gmail.com>
2024-05-22 15:59:43 -0300

    P.O.O

--- Herencia  y Plimorfismo/main.py
+++ Herencia  y Plimorfismo/main.py
@@ -1,13 +1,11 @@
 import sys
 import os
 
-# Asegúrate de que el directorio raíz del proyecto esté en el PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '')))
 
 from Marvel import HeroeMarvel, EnemigoMarvel
 from DC import HeroeDC, EnemigoDC
 
-# Ejemplo de uso
 if __name__ == "__main__":
     thor = HeroeMarvel("Thor", 1000, "Masculino", "Rompe Tormentas", "Mjolnir")
     loki = EnemigoMarvel("Loki", 1000, "Masculino", "Engaño")
